b6628134795ee5e61ee005824c3a7491c9ee8cd7
aldymartinus <18421144.aldy@gmail.com>
2026-07-20 19:15:34 +0700

    added exceeded limit

---
+++ chapter-1/functions/exceeded-limit.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#define MAXLINE 11
+	
+int get_line(char line[], int maxline);
+void copy(char to[], char from[]);
+
+int main() {
+	int len;
+	int max;
+	char line[MAXLINE];
+	char longest[MAXLINE];
+	
+	max = 0;
+
+	while((len = get_line(line, MAXLINE)) > 0) {
+		if (len > max) {
+			max = len;
+			copy(longest, line);
+		}
+	}
+
+	if(max > 0) {
+		printf("\nLongest line: %s (contain: %d chars)\n", longest, max);
+	}
+
+	return 0;
+}
+
+int get_line(char s[], int lim) {
+	int c, i;
+
+	for(i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+		s[i] = c;
+	}
+
+	if(c == '\n') {
+		s[i] = c;
+		++i;
+	}
+
+	s[i] = '\0';
+
+	if(i >= lim - 1 && s[i - 1] != '\n') {
+		while((c = getchar()) != EOF) {
+			++i;
+			if(c == '\n') {
+				break;
+			}
+		}
+	}
+	
+	return i;
+}
+
+void copy(char to[], char from[]) {
+	int i = 0;
+
+	while((to[i] = from[i]) != '\0') {
+		++i;
+	}
+}
